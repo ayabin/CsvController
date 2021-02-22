37ef79ae580d3f2dd384825a8d41e1dab0c2f2bd
ayabin <ayabin.jp@gmail.com>
2021-02-22 10:30:50 +0900

    action/update.php内のechoと変数を削除
    デバッグ時に使用したほか、headerの前で表示させていたため

--- action/update.php
+++ action/update.php
@@ -4,6 +4,6 @@ require_once('../CsvController.class.php');
 
 $POST=filter_var_array($_POST,FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 $cc=new CsvController("../".DATA_PATH.DATA_CSV_NAME,COLUMN_NAMES);
-echo $responseRead=$cc->update($POST['id'],$POST['columnName'],$POST['changeValue']);
+$cc->update($POST['id'],$POST['columnName'],$POST['changeValue']);
 header("location:../");
 ?>
